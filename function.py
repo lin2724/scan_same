f08d97a262392f35fb33e1ba2efbe669051c394d
lin2724 <lin2724@msn.com>
2017-03-20 21:24:30 +0800

    fix bug (wrong info stored to db), improve db performance

--- function.py
+++ function.py
@@ -16,6 +16,8 @@ gStoreDataBasePath = os.path.join(os.getcwd(), gDataBaseFileName)
 class SqlLibHandle:
     def __init__(self):
         self.init_flag = False
+        self.sql_count = 0
+        self.set_max_buf_sql_cnt = 200
         pass
 
     def load(self, db_file_path):
@@ -33,6 +35,7 @@ class SqlLibHandle:
           full_file_path TEXT PRIMARY KEY ,\
           hash_code CHAR ,\
           description TEXT ,\
+          file_size INT,\
           same_count INT)')
         db_handle.commit()
         db_handle.execute('CREATE INDEX hash_record_index on hash_record(full_file_path, hash_code)')
@@ -57,34 +60,45 @@ class SqlLibHandle:
             full_file_path = unicode(full_file_path.decode('gbk'))
         try:
             dup_file_list = self.check_hash_dup(full_file_path, hash_code)
+            dup_file_list = list()
             same_count = len(dup_file_list)
             if same_count != 0:
                 same_count += 1
             for dup_file_path in dup_file_list:
                 self.update_file_same_count(dup_file_path, same_count)
-            self.dbHandle.execute('INSERT OR REPLACE INTO hash_record VALUES (?,?,?,?)',
-                                (full_file_path, hash_code, description, same_count))
+            stat = os.stat(full_file_path)
+            file_size = stat.st_size
+            self.dbHandle.execute('INSERT OR REPLACE INTO hash_record VALUES (?,?,?,?,?)',
+                                (full_file_path, hash_code, description, file_size, same_count))
         except sqlite3.DataError:
             self.err_output('DataError')
             return
-        self.dbHandle.commit()
+        self.sql_count += 1
+        if self.sql_count >= self.set_max_buf_sql_cnt:
+            self.sql_count = 0
+            self.dbHandle.commit()
         pass
 
     def update_file_same_count(self, file_path, same_count):
         self.dbHandle.execute('update hash_record set same_count = (?) \
-                                where hash_record.full_file_path = (?)',
-                              (file_path, same_count))
+                                where hash_record.full_file_path == (?)',
+                              (same_count, file_path))
         self.dbHandle.commit()
+        pass
+
+    def do_commit(self):
+        if self.sql_count > 0:
+            self.dbHandle.commit ()
         pass
 
     def check_hash_dup(self, file_path, hash_code):
         dup_file_list = list()
-        con = self.dbHandle.execute('select * from hash_record where hash_record.hash_code = (?) \
+        con = self.dbHandle.execute('select * from hash_record where hash_record.hash_code == (?) \
                                   and hash_record.full_file_path != (?)', (hash_code, file_path))
         if con.rowcount != 0:
             dup_tuple_list = con.fetchall()
             for tuple_row in dup_tuple_list:
-                (file_path,_,_,_) = tuple_row
+                (file_path,_,_,_,_) = tuple_row
                 dup_file_list.append(file_path)
                 print 'append same [%s]' % file_path.decode('utf-8')
         return dup_file_list
@@ -104,7 +118,7 @@ class SqlLibHandle:
         pass
 
     def get_scan_record(self, full_folder_path):
-        con = self.dbHandle.execute('select * from scan_record where scan_record.folder_path = (?)', (full_folder_path,))
+        con = self.dbHandle.execute('select * from scan_record where scan_record.folder_path == (?)', (full_folder_path,))
         ret_dict = dict()
         if con.rowcount != 0:
             (full_folder_path, depth, time_now, state) = con.fetchall()[0]
@@ -220,22 +234,29 @@ def scan_folder_recursion(folder_path):
         if dir_depth < record_dir_depth:
             db_handle.store_scan_record(folder_scan, 0)
         start_time = time.time()
+        files_count = 0
         for dir_path, dir_names, file_names in os.walk(folder_path):
-            print 'scan [%s]' % dir_path
+            print 'scan [%s] , subdir cnt [%d] files cnt [%d]' % (dir_path, len(dir_names), len(file_names))
             dir_depth = get_dir_depth(dir_path)
             if dir_depth == record_dir_depth:
                 db_handle.store_scan_record(dir_path, 0)
             for file_name in file_names:
                 full_file_path = os.path.join(dir_path, file_name)
+                tmp_time_rec = time.time()
                 hash_code = hash_handle.get_file_hash(full_file_path)
+                #print 'get hash time use [%f]' % (time.time() - tmp_time_rec)
+                tmp_time_rec = time.time ()
                 db_handle.store_file_record(full_file_path, hash_code)
-            if dir_path == record_dir_depth:
+                #print 'store into time use [%f]' % (time.time () - tmp_time_rec)
+                files_count += 1
+            db_handle.do_commit()
+            if dir_depth == record_dir_depth:
                 db_handle.store_scan_record(dir_path, 1)
         dir_depth = get_dir_depth(folder_scan)
         if dir_depth < record_dir_depth:
             db_handle.store_scan_record(folder_scan, 1)
         rec_time = time.time() - start_time
-        print 'scan [%s] done, time use [%f]' % (folder_scan, rec_time)
+        print 'scan [%s] done, total file count [%d], time use [%f]' % (folder_scan, files_count, rec_time)
     pass
 
 
